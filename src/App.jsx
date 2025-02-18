e66c3beaf9201d00c867ba759084c2fd1762ca61
supriyabhor <supriyanaikwadi89@gmail.com>
2025-02-17 19:06:10 -0500

    initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState , useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NewsList from './components/NewsList'
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+       <h1>New Data</h1>
+       
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NewsList.jsx
@@ -0,0 +1,4 @@
+export default function NewsList(){
+
+    <h2>new list data</h2>
+}
